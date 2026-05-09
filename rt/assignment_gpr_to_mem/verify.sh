d221d21cf285b4fa672964171057b20eb7c01888
Ihor Lukianov <lis05st@gmail.com>
2026-05-09 16:45:48 +0200

    removed old tests
    Signed-off-by: Ihor Lukianov <lis05st@gmail.com>

--- rt/assignment_gpr_to_mem/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-if [ "$(./main.bin)" == "81985529216486895 2309737967 1 2 " ]; then
-    echo "Success!"
-else
-    echo "Failed!"
-    exit -1
-fi
--- rt/assignment_lit_to_gpr/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-if [ "$(./main.bin)" == "-1 4294967295 1 1 " ]; then
-    echo "Success!"
-else
-    echo "Failed!"
-    exit -1
-fi
